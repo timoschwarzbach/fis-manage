3ea62d7f3b861ee7af80567e32a2c269ed667a2d
timolated <thonphet2312@gmail.com>
2025-02-18 23:28:53 +0100

    move media page to client side for invalidating

--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -1,10 +1,29 @@
-import { api } from "~/trpc/server";
+"use client";
+
+import { api } from "~/trpc/react";
 import { UploadFile } from "./upload";
 import { Card } from "~/components/ui/card";
 import { MediaItem } from "./item";
+import { Skeleton } from "~/components/ui/skeleton";
 
-export default async function MediaPage() {
-  const files = await api.files.getAll();
+export default function MediaPage() {
+  const fileQuery = api.files.getAll.useQuery();
+  const files = fileQuery.data;
+  if (!files) {
+    return (
+      <div className="flex flex-col gap-4 p-4">
+        <div className="flex w-full flex-row items-center justify-between">
+          <span>Loading files</span>
+          <UploadFile />
+        </div>
+        <div className="flex flex-row flex-wrap items-stretch gap-4">
+          <Skeleton className="mb-20 flex aspect-video w-96 items-center justify-center" />
+          <Skeleton className="mb-20 flex aspect-video w-96 items-center justify-center" />
+          <UploadFileAsItem />
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="flex flex-col gap-4 p-4">
       <div className="flex w-full flex-row items-center justify-between">
