da8e80a5ac6f0415f038068261f56cd9c52a1f6a
timolated <thonphet2312@gmail.com>
2025-05-12 14:20:01 +0200

    add tagesschau page

--- src/components/media.tsx
+++ src/components/media.tsx
@@ -4,14 +4,17 @@ import { api } from "~/trpc/react";
 import { Skeleton } from "./ui/skeleton";
 import Image from "next/image";
 
-export function Media({ id }: { id: string }) {
+export function Media({ id, type }: { id: string; type?: "image" | "video" }) {
   const fileQuery = api.files.urlFromId.useQuery(id);
 
   if (fileQuery.isLoading) {
     return <Skeleton className="aspect-video w-full" />;
   }
 
-  if (fileQuery.data?.url && fileQuery.data.type.includes("image")) {
+  if (
+    fileQuery.data?.url &&
+    (fileQuery.data.type.includes("image") || type === "image")
+  ) {
     return (
       <Image
         className="aspect-video w-full object-contain"
@@ -23,7 +26,10 @@ export function Media({ id }: { id: string }) {
     );
   }
 
-  if (fileQuery.data?.url && fileQuery.data.type.includes("video")) {
+  if (
+    fileQuery.data?.url &&
+    (fileQuery.data.type.includes("video") || type === "video")
+  ) {
     const prefersReducedMotion = window.matchMedia(
       "(prefers-reduced-motion: reduce)",
     ).matches;
@@ -31,6 +37,53 @@ export function Media({ id }: { id: string }) {
       <video
         className="aspect-video w-full object-contain"
         src={fileQuery.data.url}
+        controls={false}
+        autoPlay={!prefersReducedMotion}
+        loop={true}
+        width={400}
+        height={300}
+      />
+    );
+  }
+
+  return <div className="aspect-video w-full rounded bg-neutral-300" />;
+}
+
+export function ExternalMedia({
+  bucketName = "fis",
+  fileName,
+  type = "image",
+}: {
+  bucketName: string;
+  fileName: string;
+  type: "image" | "video";
+}) {
+  const fileQuery = api.files.urlFromName.useQuery({ bucketName, fileName });
+
+  if (fileQuery.isLoading) {
+    return <Skeleton className="aspect-video w-full" />;
+  }
+
+  if (fileQuery.data && type === "image") {
+    return (
+      <Image
+        className="aspect-video w-full object-contain"
+        src={fileQuery.data}
+        alt="Uploaded image"
+        width={400}
+        height={300}
+      />
+    );
+  }
+
+  if (fileQuery.data && type === "video") {
+    const prefersReducedMotion = window.matchMedia(
+      "(prefers-reduced-motion: reduce)",
+    ).matches;
+    return (
+      <video
+        className="aspect-video w-full object-contain"
+        src={fileQuery.data}
         controls={false}
         autoPlay={!prefersReducedMotion}
         loop={true}
