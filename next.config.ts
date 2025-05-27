4ca171c935a5431f2031952ea9bcad97ce58b8e3
timolated <thonphet2312@gmail.com>
2025-05-27 14:44:44 +0200

    try manual old syntax allowance

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
+import path from "path";
 import "./src/env.ts";
 import { env } from "./src/env.ts";
 
@@ -10,7 +11,14 @@ const config = {
   images: {
     remotePatterns: [
       new URL(`http${env.MINIO_SSL === "true" ? "s" : ""}://${env.MINIO_ENDPOINT}:${env.MINIO_PORT}/**`),
-      new URL("http://server.fis.timoschwarzbach.de:9000/**")
+      {
+        protocol: "https",
+        hostname: "server.fis.timoschwarzbach.de",
+        port: "9000",
+        pathname: "**",
+        query: "**"
+
+      }
     ],
   },
   output: "standalone",
